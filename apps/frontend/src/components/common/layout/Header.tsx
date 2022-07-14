109790d46c55c7366156b17d0fd4cf4443cb4957
Mohammed Gehad <38791859+mohammed-gehad@users.noreply.github.com>
2022-07-15 01:48:48 +0530

    fix build

--- apps/frontend/src/components/common/layout/Header.tsx
+++ apps/frontend/src/components/common/layout/Header.tsx
@@ -1,10 +1,10 @@
-import { Header as MHeader, Text, Title } from '@mantine/core'
+import { Header as MHeader, Text } from '@mantine/core'
 import ChangeThemeIcon from '../button/ChangeThemeIcon'
 
 const Header = () => (
   <MHeader height={60} p="xs">
     <div className="flex justify-between items-center px-10">
-      <Text className="text-2xl md:text-4xl font-bold">
+      <Text className="text-2xl md:text-3xl font-bold">
         🍞 Gluten Additives
       </Text>
       <ChangeThemeIcon />
